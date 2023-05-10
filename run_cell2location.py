641c6c39d1c2bcae2ef5271420629f3669d65443
anne04 <anne.06.cse@gmail.com>
2023-05-10 16:35:49 -0400

    Update run_cell2location.py

--- run_cell2location.py
+++ run_cell2location.py
@@ -62,4 +62,95 @@ mod = RegressionModel(adata_ref)
 mod.view_anndata_setup()
 mod.train(max_epochs=250, use_gpu=True)
 
+#mod.plot_history(20)
+
+# In this section, we export the estimated cell abundance (summary of the posterior distribution).
+adata_ref = mod.export_posterior(
+    adata_ref, sample_kwargs={'num_samples': 1000, 'batch_size': 2500, 'use_gpu': True}
+)
+
+# Save model
+mod.save(f"{ref_run_name}", overwrite=True)
+
+# Save anndata object with results
+adata_file = f"{ref_run_name}/sc.h5ad"
+adata_ref.write(adata_file)
+adata_file
+'''
+adata_ref = mod.export_posterior(
+    adata_ref, use_quantiles=True,
+    # choose quantiles
+    add_to_obsm=["q05","q50", "q95", "q0001"],
+    sample_kwargs={'batch_size': 2500, 'use_gpu': True}
+)
+
+mod.plot_QC()
+'''
+'''
+adata_file = f"{ref_run_name}/sc.h5ad"
+adata_ref = sc.read_h5ad(adata_file)
+mod = cell2location.models.RegressionModel.load(f"{ref_run_name}", adata_ref)
+'''
+
+
+if 'means_per_cluster_mu_fg' in adata_ref.varm.keys():
+    inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_{i}'
+                                    for i in adata_ref.uns['mod']['factor_names']]].copy()
+else:
+    inf_aver = adata_ref.var[[f'means_per_cluster_mu_fg_{i}'
+                                    for i in adata_ref.uns['mod']['factor_names']]].copy()
+inf_aver.columns = adata_ref.uns['mod']['factor_names']
+inf_aver.iloc[0:5, 0:5]
+
+
+#__________________________________________________________________________________________
+# find shared genes and subset both anndata and reference signatures
+intersect = np.intersect1d(adata_vis.var_names, inf_aver.index)
+adata_vis = adata_vis[:, intersect].copy()
+inf_aver = inf_aver.loc[intersect, :].copy()
+
+# prepare anndata for cell2location model
+cell2location.models.Cell2location.setup_anndata(adata=adata_vis, batch_key="sample")
+# create and train the model
+mod = cell2location.models.Cell2location(
+    adata_vis, cell_state_df=inf_aver,
+    # the expected average cell abundance: tissue-dependent
+    # hyper-prior which can be estimated from paired histology:
+    N_cells_per_location=30,
+    # hyperparameter controlling normalisation of
+    # within-experiment variation in RNA detection:
+    detection_alpha=20
+)
+mod.view_anndata_setup()
+mod.train(max_epochs=30000,
+          # train using full data (batch_size=None)
+          batch_size=None,
+          # use all data points in training because
+          # we need to estimate cell abundance at all locations
+          train_size=1,
+          use_gpu=True,
+         )
+
+#mod.plot_history(1000)
+#plt.legend(labels=['full data training']);
+
+# In this section, we export the estimated cell abundance (summary of the posterior distribution).
+adata_vis = mod.export_posterior(
+    adata_vis, sample_kwargs={'num_samples': 1000, 'batch_size': mod.adata.n_obs, 'use_gpu': True}
+)
+
+# Save model
+mod.save(f"{run_name}", overwrite=True)
+
+# mod = cell2location.models.Cell2location.load(f"{run_name}", adata_vis)
+
+# Save anndata object with results
+adata_file = f"{run_name}/sp.h5ad"
+adata_vis.write(adata_file)
+adata_file
+'''
+adata_file = f"{run_name}/sp.h5ad"
+adata_vis = sc.read_h5ad(adata_file)
+mod = cell2location.models.Cell2location.load(f"{run_name}", adata_vis)
+'''
 
